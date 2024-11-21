d042af52500caed529aa2cfda02c7340616ba0b1
Tan Choon Peng <TAN_Choon_Peng@np.edu.sg>
2024-11-21 16:15:29 +0800

    Complete Week 6's practical

--- wk06/CAs/1 Wk06_ JS Basics Recap/ca-Print-array/script.js
+++ wk06/CAs/1 Wk06_ JS Basics Recap/ca-Print-array/script.js
@@ -8,7 +8,7 @@ for (let i = 0; i < a.length; ++i) {
     //add class to element through javascript
     newDiv.classList.add("mystyle")
     // and give it some content
-    let newContent = document.createTextNode(""+ a[i]);
+    let newContent = document.createTextNode("" + a[i]);
     // add the text node to the newly created div
     newDiv.appendChild(newContent);
     //add the div to the body of the html
--- wk06/CAs/2 Wk06_ JS Objects/ca-Literal-Object/script.js
+++ wk06/CAs/2 Wk06_ JS Objects/ca-Literal-Object/script.js
@@ -13,7 +13,9 @@ let newDiv = document.createElement("div");
 //add class to element through javascript
 newDiv.classList.add("mystyle")
 // and give it some content
-newDiv.innerHTML = "Name: " + momobae.name + "<br>Specialty: " + momobae.specialty + "<br>Power: " + momobae.power + "<br>Hitpoints: " + momobae.hitpoints + "<br>Level: " + momobae.level + "<br>Gender: " + momobae.gender;
+newDiv.innerHTML = "Name: " + momobae.name + "<br>Specialty: " + momobae.specialty + "<br>Power: " 
++ momobae.power + "<br>Hitpoints: " + momobae.hitpoints + "<br>Level: " 
++ momobae.level + "<br>Gender: " + momobae.gender;
 //add the div to the body of the html
 document.body.appendChild(newDiv);
 
